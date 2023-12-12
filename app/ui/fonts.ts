6ba04f8fa5bb59d6d4f6c80ad5e1f06afaa6c666
Rizal Rahman <rizalrahman@Jihans-MacBook-Air.local>
2023-12-12 15:14:58 +0800

    feat: fetch dashboard data using vercel postgres

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,12 +2,10 @@ import { Inter, Lusitana } from 'next/font/google';
  
 const inter = Inter({
     subsets: ['latin'],
-    variable: '--font-inter',
   })
    
   const lusitana = Lusitana({
       subsets: ['latin'],
-      variable: '--font-lusitana',
       weight: '400'
   })
 
